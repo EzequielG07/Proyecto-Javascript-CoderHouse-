766d70bce685993f4d6172ae2ea64cca7a600882
Ezequiel Guaymas <ezequiel.guaymas07@gmail.com>
2022-10-23 23:19:01 -0300

    Se esta probando modal - realizar pruebas del onclic dentro de la funcion

--- indexNew.js
+++ indexNew.js
@@ -1,13 +1,27 @@
 let contenedorHabitaciones;
+let formulario;
+let botonesCerrarModalAgregarReserva;
+let modalAddReserva;
+let botonAgregarReserva;
+let modal;
 
 const arrayHabitaciones = [
   { id: 1, nombre: "Normal", precio: 1000, personas: 1 },
-  { id: 2, nombre: "Matrimonial", precio: 2000, personas: 2 },
-  { id: 3, nombre: "Familiar", precio: 4000, personas: 4 },
+  /* { id: 2, nombre: "Matrimonial", precio: 2000, personas: 2 },
+  { id: 3, nombre: "Familiar", precio: 4000, personas: 4 },*/
 ];
 
 function inicializarElementos() {
   contenedorHabitaciones = document.getElementById("containerHabitaciones");
+  formulario = document.getElementById("formulario");
+
+  botonesCerrarModalAgregarReserva = document.getElementsByClassName(
+    "btnCerrarModalAgregarReserva"
+  );
+  modalAddReserva = document.getElementById("modalAddReserv");
+  botonAgregarReserva = document.getElementById("btnAgregarReserva");
+
+  modal = new bootstrap.Modal(modalAddReserva);
 }
 
 function mostrarHabitaciones() {
@@ -21,7 +35,7 @@ function mostrarHabitaciones() {
       <p class="card-text textExample"><b>Tipo de Habitación:</b> ${HABITACIONES.nombre}</p>
       <p class="card-text textExample"><b>Capacidad de Personas:</b> ${HABITACIONES.personas}</p>
       <p class="card-text textExample"><b>Precio por día:</b> $${HABITACIONES.precio}</p>
-      </div>
+            </div>
       </div>
       `;
 
@@ -29,5 +43,19 @@ function mostrarHabitaciones() {
   }
 }
 
+function inicializarEventos() {
+  botonAgregarReserva.onclick = abrirModalAgregarReserva;
+}
+
+function abrirModalAgregarReserva() {
+  modal.show();
+}
+
+/*function cerrarModalAgregarReserva() {
+  formulario.reset();
+  modal.hide();
+}*/
+
 inicializarElementos();
 mostrarHabitaciones();
+inicializarEventos();
