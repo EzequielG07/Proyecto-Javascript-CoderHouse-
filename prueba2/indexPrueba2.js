a5a7e815316e965199d2b36716ab9e296e0bb9f3
EzequielG <ezequiel.guaymas07@gmail.com>
2022-10-26 16:44:07 -0300

    Se realizo una nueva prueba incorporando nuevos archivos y dejando la posibilidad de seleccionar

--- prueba2/indexPrueba2.js
+++ prueba2/indexPrueba2.js
@@ -1,22 +1,28 @@
-let contenedorHabitaciones;
-let formulario;
-let botonesCerrarModalAgregarReserva;
-let modalAddReserva;
-let modal;
+/* DESAFIO - Fetch en tu proyecto
+Para este desafio se agrego una nuevo objeto de lugares y utilizando mockapi se preparo un total de 10 objetos ramdom que son llamados al momento de iniciar el simulador, se aclara que es texto dummy pero cumple de ser una funcion asincrona*/
 
+let contenedorHabitaciones;
 let contenedorReserva;
+let contenedorSeleccion;
+let contenedorLugares;
+let formulario;
 let inputId;
 let inputApellido;
 let inputEmail;
 let inputCel;
 let inputDias;
-let getPrecio;
 let reservas = [];
+let lugares = [];
+
+let botonesCerrarModalAgregarReserva;
+let modalAddReserva;
+let modal;
+let getPrecio;
 
 const arrayHabitaciones = [
   { id: 1, nombre: "Normal", precio: 1000, personas: 1 },
-  /* { id: 2, nombre: "Matrimonial", precio: 2000, personas: 2 },
-  { id: 3, nombre: "Familiar", precio: 4000, personas: 4 },*/
+  { id: 2, nombre: "Matrimonial", precio: 2000, personas: 2 },
+  { id: 3, nombre: "Familiar", precio: 4000, personas: 4 },
 ];
 
 class Reserva {
@@ -30,23 +36,54 @@ class Reserva {
   }
 }
 
+class Lugares {
+  constructor(id, nombre, cuartosDisp, telefono, fechas) {
+    this.id = id;
+    this.nombre = nombre.toUpperCase();
+    this.cuartosDisp = cuartosDisp;
+    this.telefono = telefono;
+    this.fechas = fechas;
+  }
+}
+
+function pintarLugares() {
+  contenedorLugares.innerHTML = "";
+  lugares.forEach((dato) => {
+    let column = document.createElement("div");
+    column.className = "col-md-auto mt-3 ms-3";
+    column.id = `columnaLugares-${dato.id}`;
+    column.innerHTML = `
+   <div class="card">
+      <div class="card-body">
+         <p class="card-text textLugares"> <b>ID: </b>${dato.id}</p>
+         <p class="card-text textLugares"> <b>Lugar: </b>${dato.nombre}</p>
+         <p class="card-text textLugares"> <b>Cuartos Disponibles: </b>${dato.cuartosDisp}</p>
+         <p class="card-text textLugares"><b>Tel: </b> ${dato.telefono}</p>
+         <p class="card-text textLugares"><b>Fecha: </b> ${dato.fechas} </p>
+      </div>
+   </div>
+`;
+    contenedorLugares.append(column);
+  });
+}
+
 function inicializarElementos() {
   contenedorHabitaciones = document.getElementById("containerHabitaciones");
+  contenedorReserva = document.getElementById("containerReserva");
+  contenedorLugares = document.getElementById("containerLugares");
+  contenedorSeleccion = document.getElementById("containerSeleccion");
   formulario = document.getElementById("formulario");
+  inputId = document.getElementById("inputId");
+  inputApellido = document.getElementById("inputApellido");
+  inputEmail = document.getElementById("inputEmail");
+  inputCel = document.getElementById("inputCel");
+  inputDias = document.getElementById("inputDias");
 
   botonesCerrarModalAgregarReserva = document.getElementsByClassName(
     "btnCerrarModalAgregarReserva"
   );
   modalAddReserva = document.getElementById("modalAddReserv");
   modal = new bootstrap.Modal(modalAddReserva);
-
-  contenedorReserva = document.getElementById("containerReserva");
-  inputId = document.getElementById("inputId");
-  inputApellido = document.getElementById("inputApellido");
-  inputEmail = document.getElementById("inputEmail");
-  inputCel = document.getElementById("inputCel");
-  inputDias = document.getElementById("inputDias");
-  /*getPrecio = document.getElementById("getPrecio");*/
   getPrecio = document.getElementById("getPrecio");
 }
 
@@ -60,16 +97,50 @@ function mostrarHabitaciones() {
       <div class="card-body">
       <p class="card-text textExample"><b>Tipo de Habitación:</b> ${HABITACIONES.nombre}</p>
       <p class="card-text textExample"><b>Capacidad de Personas:</b> ${HABITACIONES.personas}</p>
-      <p class="card-text textExample"><b>Precio por día:</b>
-      <p class="card-text textExample" id="precioXdia">${HABITACIONES.precio}</p></p>
-           <div class="card-footer">
+      <p class="card-text textExample"><b>Precio por día:</b> $${HABITACIONES.precio}</p>
+               <div class="card-footer">
+               <button class="btn btn-success" id="botonSeleccionar-${HABITACIONES.id}" >Seleccionar</button>
+                  <button class="btn btn-danger" id="botonEliminar-${HABITACIONES.id}" >Eliminar</button>
+         </div>
+      </div>
+      </div>
+      `;
+
+    contenedorHabitaciones.append(section);
+
+    let botonSeleccionar = document.getElementById(
+      `botonSeleccionar-${HABITACIONES.id}`
+    );
+    let botonEliminar = document.getElementById(
+      `botonEliminar-${HABITACIONES.id}`
+    );
+
+    botonSeleccionar.onclick = () => seleccionarHab(HABITACIONES);
+    botonEliminar.onclick = () => eliminarHab(HABITACIONES);
+  }
+}
+
+function seleccionarHab(arrayHabs) {
+  if (document.getElementById(`columnaNewHab-${arrayHabs.id}`)) {
+    mostrarMensaje("YA SELECCIONO ESTA HABITACION");
+  } else {
+    let newSection = document.createElement("div");
+    newSection.className = "mt-3";
+    newSection.id = `columnaNewHab-${arrayHabs.id}`;
+    newSection.innerHTML = `
+      <div class="card">
+      <div class="card-body">
+      <p class="card-text textExample"><b>Tipo de Habitación:</b> ${arrayHabs.nombre}</p>
+      <p class="card-text textExample"><b>Capacidad de Personas:</b> ${arrayHabs.personas}</p>
+       <p class="card-text textExample" id="precioXdia">${arrayHabs.precio}</p></p>
+      <div class="card-footer">
                <button class="btn btn-success" id="btnAgregarReserva">Agregar Reserva</button> 
             </div>
       </div>
       </div>
       `;
 
-    contenedorHabitaciones.append(section);
+    contenedorSeleccion.append(newSection);
 
     let botonAgregarReserva = document.getElementById("btnAgregarReserva");
 
@@ -79,6 +150,26 @@ function mostrarHabitaciones() {
 
 function abrirModalAgregarReserva() {
   modal.show();
+}
+
+function eliminarHab(arrayHabs) {
+  const consulta = document.getElementById(`columnaNewHab-${arrayHabs.id}`)
+    ? true
+    : false;
+
+  consulta
+    ? document.getElementById(`columnaNewHab-${arrayHabs.id}`).remove()
+    : mostrarMensaje("Aun no selecciono esta Habitación");
+}
+
+function mostrarMensaje(mensaje) {
+  Toastify({
+    text: mensaje,
+    duration: 3000,
+    close: true,
+    gravity: "top",
+    position: "right",
+  }).showToast();
 }
 
 function inicializarEventos() {
@@ -185,6 +276,21 @@ function obtenerReservasStorage() {
   }
 }
 
+async function consultarLugaresServer() {
+  try {
+    const response = await fetch(
+      "https://6347210b04a6d45757a114c4.mockapi.io/api/v1/lugares"
+    );
+    const data = await response.json();
+    lugares = [...data];
+    pintarLugares();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 inicializarElementos();
-mostrarHabitaciones();
 inicializarEventos();
+mostrarHabitaciones();
+obtenerReservasStorage();
+consultarLugaresServer();
